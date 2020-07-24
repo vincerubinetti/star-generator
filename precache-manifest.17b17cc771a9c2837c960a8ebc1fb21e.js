130318f182e2282b6b5d50c05c8309853e8b6348
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-07-23 22:03:59 -0400

    deploy

---
+++ precache-manifest.17b17cc771a9c2837c960a8ebc1fb21e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d1e43cf0564ec38dd2f7057b3fe309",
+    "url": "/star-generator/index.html"
+  },
+  {
+    "revision": "ca4d31567517c63dde70",
+    "url": "/star-generator/static/css/main.b3a35896.chunk.css"
+  },
+  {
+    "revision": "9cf88f3e395b0c76b18b",
+    "url": "/star-generator/static/js/2.8a1e4d6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-generator/static/js/2.8a1e4d6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca4d31567517c63dde70",
+    "url": "/star-generator/static/js/main.e66d1e23.chunk.js"
+  },
+  {
+    "revision": "d16b3c6516cb8eceb694",
+    "url": "/star-generator/static/js/runtime-main.c20930e1.js"
+  }
+]);
